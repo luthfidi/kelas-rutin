3e88f8168e1e7648f5f25b5c51ec5968d7eef9fc
Luthfi <luthfihadi543@gmail.com>
2025-06-28 21:07:58 +0700

    meet 7

---
+++ meet7-notes/zk-age-verification/src/config/chains.ts
@@ -0,0 +1,26 @@
+import type { Chain } from 'wagmi/chains'
+
+export const monadTestnet: Chain = {
+  id: 10143,
+  name: 'Monad Testnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'MON',
+    symbol: 'MON',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://testnet-rpc.monad.xyz/'],
+    },
+    public: {
+      http: ['https://testnet-rpc.monad.xyz/'],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: 'MonadScan',
+      url: 'https://testnet.monadexplorer.com',
+    },
+  },
+  testnet: true,
+}
